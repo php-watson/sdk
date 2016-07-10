4631495b8416ded9f55acd15318093e99a68c40d
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-09 22:04:47 -0300

    Add Response methods to interface

---
+++ src/Interfaces/ResponseInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PhpWatson\Sdk\Interfaces;
+
+use GuzzleHttp\Psr7\Response;
+
+interface ResponseInterface
+{
+    /**
+     * Parse an incoming PSR7 response
+     *
+     * @param Response $response
+     * @return mixed
+     */
+    public function parse(Response $response);
+
+    /**
+     * Return true if the request produces an error
+     *
+     * @return bool
+     */
+    public function isError();
+
+    public function getContent();
+
+    public function setContent($content);
+
+    public function getHeaders();
+
+    public function setHeaders($headers);
+
+    public function getStatusCode();
+
+    public function setStatusCode($statusCode);
+
+    public function setIsError($isError);
+}
